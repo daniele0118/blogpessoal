ff6447c994345c18d05d64a64ed5c670a67834e5
daniele0118 <aheipo@yahoo.com.br>
2024-05-27 14:43:20 -0300

    Corrigindo erros

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -4,9 +4,10 @@ import { Postagem } from "./entities/postagem.entity";
 import { PostagemService } from "./services/postagem.service";
 import { PostagemController } from "./controllers/postagem.controller";
 import { TemaService } from "../tema/services/tema.service";
+import { TemaModule } from "../tema/tema.module";
 
     @Module({
-        imports: [TypeOrmModule.forFeature([Postagem])],
+        imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
         providers: [PostagemService, TemaService],
         controllers: [PostagemController],
         exports: [TypeOrmModule]
